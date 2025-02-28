0be0d53a1c6236460fc51d25fdb1600c9d4dc70e
kevindakin <142453268+kevindakin@users.noreply.github.com>
2025-02-28 04:12:44 -0800

    v1.4

--- blog-carousel.js
+++ blog-carousel.js
@@ -1,35 +1,38 @@
 function blogCarousel() {
-    const wrapper = document.querySelector(".blog-carousel_component");
-    const slider = wrapper.querySelector(".blog-carousel_cms.swiper");
-    const arrowPrev = wrapper.querySelector(".blog-carousel_arrow.swiper-prev");
-    const arrowNext = wrapper.querySelector(".blog-carousel_arrow.swiper-next");
+  const wrapper = document.querySelector(".blog-carousel_component");
 
-    if (!wrapper) {
-      return;
-    }
-  
-    let swiper = new Swiper(slider, {
-      slidesPerView: 1.2,
-      spaceBetween: 16,
-      speed: 300,
-      loop: true,
-      loopFillGroupWithBlank: true,
-      centeredSlides: true,
-      watchOverflow: true,
-      navigation: {
-        nextEl: arrowNext,
-        prevEl: arrowPrev,
-      },
-      breakpoints: {
-        768: {
-          slidesPerView: 2,
-        },
-        992: {
-          slidesPerView: "auto",
-          spaceBetween: 24,
-        },
-      },
-    });
+  if (!wrapper) return;
+
+  const slider = wrapper.querySelector(".blog-carousel_cms.swiper");
+  const arrowPrev = wrapper.querySelector(".blog-carousel_arrow.swiper-prev");
+  const arrowNext = wrapper.querySelector(".blog-carousel_arrow.swiper-next");
+
+  if (!slider || slider.offsetWidth === 0 || slider.offsetHeight === 0) {
+    return;
   }
-  
-  blogCarousel();  
+
+  let swiper = new Swiper(slider, {
+    slidesPerView: 1.2,
+    spaceBetween: 16,
+    speed: 300,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    centeredSlides: true,
+    watchOverflow: true,
+    navigation: {
+      nextEl: arrowNext,
+      prevEl: arrowPrev,
+    },
+    breakpoints: {
+      768: {
+        slidesPerView: 2,
+      },
+      992: {
+        slidesPerView: "auto",
+        spaceBetween: 24,
+      },
+    },
+  });
+}
+
+blogCarousel();
